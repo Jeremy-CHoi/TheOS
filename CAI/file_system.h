3871de25b9b785f92947ab779e29cb927e59b2aa
Jeremy-CHoi <117010005@link.cuhk.edu.cn>
2020-05-04 23:26:52 +0800

    Correct link error for further usage

--- CAI/file_system.h
+++ CAI/file_system.h
@@ -25,19 +25,18 @@ void show_dir(char dir_name[]);
 int create_dir(char dir_name[]);
 int change_dir(char dir_name[]);
 int delete_dir(char file_name[]);
+
 int change_name(char pre_name[], char new_name[]);
 
 int creat_fcb(int fcb_block_number, int file_block_number, int file_size);
 int add_dir_unit(dir_table* system_dir_table, char file_name[], int type, int fcb_block_number);
+int delete_dir_unit(dir_table* current_dir_tabe, int unit_index);
 
-int find_unit(dir_table* system_dir_table, char unit_name[]);
+int find_unit_in_table(dir_table* system_dir_table, char unit_name[]);
 
 int delete_file(char file_name[]);
 int release_file(int fcb_block);
 int delete_file_in_table(dir_table* system_dir_table, int unit_index);
-int delete_dir_unit(dir_table* current_dir_tabe, int unit_index);
-
-int find_unit_in_table(dir_table* current_dir_table, int unit_index);
 
 int change_dir(char dir_name[]);
 
